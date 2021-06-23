b1c3a12b710fcacd7c91b12e21915ad06c33236f
frankmao666 <frankmao666@gmail.com>
2021-06-23 13:40:09 -0400

    fix breakage in app.py

--- backend/app.py
+++ backend/app.py
@@ -14,8 +14,9 @@ def noise_endpoint(model):
     pil_img = Image.open(imageFile)
     # TODO we may need to change result, it is now a tuple
     result = add_noise(pil_img, model)
+    noised_image = result[0]
     img_io = io.BytesIO()
-    result.save(img_io, 'PNG')
+    noised_image.save(img_io, 'PNG')
     img_io.seek(0)
     return send_file(img_io, mimetype='image/png')
 
