363d19f59c23329ea69ebb82d297630d5f28b601
Frank Shi <50431556+fshipy@users.noreply.github.com>
2021-07-01 22:28:46 -0400

    migrate notebook code to NoiseService.py (#3)
    * migrate notebook code to NoiseService.py
    
    * fix breakage in app.py
    
    * modify app.py to add hard-coded target and label file path

--- backend/app.py
+++ backend/app.py
@@ -12,9 +12,13 @@ app = Flask(__name__)
 def noise_endpoint(model):
     imageFile = flask.request.files.get('image', '')
     pil_img = Image.open(imageFile)
-    result = add_noise(pil_img, model)
+    # TODO we may need to change result, it is now a tuple
+    target = 2 # TODO update it to user input?
+    label_file = "imagenet.txt" # TODO update it to be the real path
+    result = add_noise(pil_img, model, label_file, target)
+    noised_image = result[0]
     img_io = io.BytesIO()
-    result.save(img_io, 'PNG')
+    noised_image.save(img_io, 'PNG')
     img_io.seek(0)
     return send_file(img_io, mimetype='image/png')
 
