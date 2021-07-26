ebf5f610a1c1fea177dc5e107b0fd4f65997869e
Ayman Kassab <aymkas@gmail.com>
2021-07-26 15:05:37 -0400

    Create Try It Form (#12)
    * Create Try It page Bootstrap form
    
    * Create form validation
    
    * Add Access-Control-Allow-Origin header
    
    * Pipe target class from user
    - Generate random target class upon target="random"
    
    * Handle POST /noise/<model> status 200

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,28 @@
 
 import flask
+import random
 from flask import Flask
 from PIL import Image
+from random import seed
+from random import randint
 
 from backend.NoiseService import add_noise
 from backend.dto import Response, ResponseImages, ResponsePredications, encodeImage
 
 app = Flask(__name__)
-
+seed(1)
 
 @app.route('/noise/<model>', methods=['POST'])
 def noise_endpoint(model):
     imageFile = flask.request.files.get('image', '')
     pil_img = Image.open(imageFile)
 
-    target = 2  # TODO update it to user input?
+    target = flask.request.form.get('target', '')
+    if target == "random":
+        target = random.randint(0, 999)
+    else:
+        target = int(target)
+
     label_file = "imagenet.txt"  # TODO update it to be the real path
     result = add_noise(pil_img, model, label_file, target)
 
@@ -26,24 +34,28 @@ def noise_endpoint(model):
     class_noised = result[5]
     confidence_noised = result[6]
 
-    return {
-
-        'predictions:': {
-            'original': {
-                'class': class_no_noise,
-                'confidence': confidence_no_noise
+    response = flask.jsonify(
+        {
+            'predictions': {
+                'original': {
+                    'class': class_no_noise,
+                    'confidence': confidence_no_noise
+                },
+                'noised': {
+                    'class': class_noised,
+                    'confidence': confidence_noised
+                }
             },
-            'noised': {
-                'class': class_noised,
-                'confidence': confidence_noised
+            'images': {
+                'noised': encodeImage(noised_image),
+                'original': encodeImage(original_image),
+                'pure_noise': encodeImage(pure_noise)
             }
-        },
-        'images': {
-            'noised': encodeImage(noised_image),
-            'original': encodeImage(original_image),
-            'pure_noise': encodeImage(pure_noise)
         }
-    }
+    )
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.status_code = 200
+    return response
 
 
 if __name__ == '__main__':
